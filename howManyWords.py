e7c73f2478c70861d862d87e995f4960f75f96a4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-06 13:56:12 -0500

    Updated from Brython Server: 12/6/2017 1:56:18 PM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,12 @@
+#Emily Murphy
+#2017-12-06
+#howManyWords.py - how many words there are for each number of letters
 
+dictionary = open('engmix.txt')
+
+L = []
+for word in dictionary:
+    if 'e' in word:
+        L.append(word)
+
+print(len(L))
