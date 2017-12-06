5aa05b8dfa0c3daa5965578a0daa6a2755e20472
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-06 13:53:52 -0500

    Updated from Brython Server: 12/6/2017 1:53:58 PM

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,12 @@
+#Emily Murphy
+#2017-12-06
+#zzwords.py - prints words that contain 'zz'
 
+dictionary = open('engmix.txt')
+
+wordCount = 0
+for word in dictionary:
+    if 'zz' in word:
+        print(word.strip()) #gets rid of weird spacing after each word
+    wordCount += 1
+    
