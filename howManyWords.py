6c997a191f375169ed019a3606ce3ccd7f55b596
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-07 14:17:20 -0500

    Updated from Brython Server: 12/7/2017 2:17:26 PM

--- howManyWords.py
+++ howManyWords.py
@@ -6,10 +6,10 @@ dictionary = open('engmix.txt')
 
 L = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 for word in dictionary:
-    L[(len(word))-1] += 1
+    L[len(word)-1] += 1
  
 i = 0   
-while i <= len(word):
-    i += 2  
+while i < len(word):
+    i += 1  
     print('There are', L[i], 'words with', i, 'letters')
       
