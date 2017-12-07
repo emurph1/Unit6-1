e2d1d528a862c6beafb633a446b08d55dc9ec6dc
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-07 14:33:13 -0500

    Updated from Brython Server: 12/7/2017 2:33:18 PM

--- reverseFile.py
+++ reverseFile.py
@@ -2,12 +2,10 @@
 #2017-12-06
 #reverseFile.py - input file name and prints lines in file in reverse order
 
-fileName = input('Enter file name: ')
-file = open(fileName)
+file = open(input('Enter file name: '))
 L = []
-for word in fileName:
-    L.append(word)
+for line in file:
+    L.append(line)
     L.reverse()
-    
-print(L)
+    print(L)
     
