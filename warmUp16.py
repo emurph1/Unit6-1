d5f232435b38976de5fac9cb703ac6aa8ffc82a9
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-11 14:32:05 -0500

    Updated from Brython Server: 12/11/2017 2:32:11 PM

--- warmUp16.py
+++ warmUp16.py
@@ -3,9 +3,7 @@
 #warmUp16.py 
 
 dictionary = open('engmix.txt')
-L = []
+
 for line in dictionary:
-    if line[0] == 'e' and line[-1] == 'y': 
-        L.append(line.strip())
-  
-print(line)
+    if line[0] == 'e' and line[-1] == 'm': 
+        print(line.strip())
