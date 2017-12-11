805820523833ca5edd0911edc8b71bca43121a45
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-11 14:53:25 -0500

    Updated from Brython Server: 12/11/2017 2:53:32 PM

--- palindromes.py
+++ palindromes.py
@@ -3,11 +3,12 @@
 #palindromes.py - prints out all palindromes in the dictionary
 
 dictionary = open('palindromes.txt')
-L = []
+
 for word in dictionary:
     word = word.strip()
     if word != '':
+        L = []
         for ch in word:
             L.append(ch)
-            if L == L.reverse():
-                print(word)
+            
+
