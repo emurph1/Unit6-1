7eb27716067c48587feaac069395a93efafeb874
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-13 13:27:40 -0500

    Updated from Brython Server: 12/13/2017 1:27:43 PM

--- quiz6.py
+++ quiz6.py
@@ -4,9 +4,14 @@
 
 D = open('engmix.txt')
 
-for word in D:
+'''for word in D:
     if word.count('c') == 3:
-        print(word)
+        print(word.strip())
     elif word.count('p') == 2:
-        print(word)
+        print(word.strip())'''
+        
+for word in D:
+    if word != '':
+        if word[0] == 'r':
+            print(word)
         
