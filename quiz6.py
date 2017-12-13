0b60d04e2e9c2fd6916e035fcb7c10ba3a0f7b21
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-13 13:25:34 -0500

    Updated from Brython Server: 12/13/2017 1:25:36 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,12 @@
+#Emily Murphy
+#12/13/17
+#quiz6.py
 
+D = open('engmix.txt')
+
+for word in D:
+    if word.count('c') == 3:
+        print(word)
+    elif word.count('p') == 2:
+        print(word)
+        
