d38608c1f46a1348ff1e594ce713740557c0e0c0
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-14 14:40:47 -0500

    Updated from Brython Server: 12/14/2017 2:40:52 PM

--- project2.py
+++ project2.py
@@ -1,1 +1,20 @@
+#Emily Murphy
+#2017-12-14
+#project2.py - Lexicant (word guessing game)
 
+from random import randint
+
+#loads all words from dictionary (D) to a list (DList)
+def loadD():
+    D = open('engmix.txt')
+    DList = []
+    for word in D:
+        DList.append(word.trip())
+    return DList
+    
+
+if __name__ == '__main__': 
+    lettersL = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    randL = lettersL[randint(1,26)]
+    
+print(randL)
