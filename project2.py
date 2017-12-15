dad5275be1614a6622e6bf1716e0fe2a5420851e
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-15 14:47:04 -0500

    Updated from Brython Server: 12/15/2017 2:47:10 PM

--- project2.py
+++ project2.py
@@ -30,6 +30,7 @@ if __name__ == '__main__':
     data = {}
     data['pN'] = ''
     data['cL'] = ''
+    data['oP'] = ''
     lettersL = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     randL = lettersL[randint(1,26)-1]
     print('Starting letter: ', randL)
