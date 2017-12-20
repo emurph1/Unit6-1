fbbff8ea06831025bace9e6387bc5fc062486060
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-20 13:30:58 -0500

    Updated from Brython Server: 12/20/2017 1:31:02 PM

--- project2.py
+++ project2.py
@@ -18,16 +18,8 @@ def updateLetter(cL,nL,s):
     s = input('before or after? ')
     if s == 'before':
         print(nL + randL)
-        data['cL'] += str(nL) + str(randL)
-        data['cL'] = cL
-        if len(data['cL']) == 3:
-            challenge(1,2,3)
     elif s == 'after':
         print(randL + nL)
-        data['cL'] += str(randL) + str(nL)
-        data['cL'] = cL
-        if len(data['cL']) == 3:
-            challenge(1,2,3)
     data['p'] = 'Player 2'
  
 #defines what a challenge is and does it 
@@ -47,14 +39,13 @@ def challenge(cL,pN,oP):
         
 #main code that generates the "board" and displays the letters (starting and input)
 if __name__ == '__main__':
-    data = {}
-    data['pN'] = ''
-    data['cL'] = ''
-    data['oP'] = ''
-    data['p'] = 'Player 1'
     lettersL = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     randL = lettersL[randint(1,26)-1]
     print('Starting letter: ', randL)
     updateLetter(1,2,3)
+    if len(cL) == 3:
+        challege(1,2,3)
+    elif len(data['cL']) == 3:
+        challenge(1,2,3)
     
     
