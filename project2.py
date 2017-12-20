7df82fefa6084b679de9983ad492475b497ab950
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-20 17:01:18 -0500

    Updated from Brython Server: 12/20/2017 5:01:17 PM

--- project2.py
+++ project2.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
         p = 'Player 1'
         oP = 'Player 2'
         updateLetter(randL,2,3)
+        print(cL)
         if len(cL) == 3:
             challege(cL,p,oP)
         elif len(cL) == 3:
