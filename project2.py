7679e91c87917daea389eb4721f6d5b87c362c2a
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-20 13:44:21 -0500

    Updated from Brython Server: 12/20/2017 1:44:26 PM

--- project2.py
+++ project2.py
@@ -36,11 +36,12 @@ if __name__ == '__main__':
     lettersL = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     randL = lettersL[randint(1,26)-1]
     print('Starting letter: ', randL)
+    cL = randL
     while True:
         p = 'Player 1'
         oP = 'Player 2'
-        cL = randL
         updateLetter(cL,2,3)
+        print(cL)
         if len(cL) == 3:
             challege(cL,p,oP)
         elif len(cL) == 3:
