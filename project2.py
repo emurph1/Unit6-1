c16cb4556d731c80b2a0a6d90dd775bdf4b62dd4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-12-30 11:36:40 -0500

    Updated from Brython Server: 12/30/2017 11:36:40 AM

--- project2.py
+++ project2.py
@@ -36,14 +36,14 @@ if __name__ == '__main__':
     while True:
         s = input('before or after? ')
         for i in range(0,1):
-            p = "Player 1"
             nL = input( p + ' enter a letter: ')
+            p = "Player 1"
             oP = "Player 2"
             cL = updateLetter(cL,nL,s)
             print(cL)
         for i in range(0,1):
-            p = "Player 2"
             nL = input( p + ' enter a letter: ')
+            p = "Player 2"
             oP = "Player 1"
             cL = updateLetter(cL,nL,s)
             print(cL)
