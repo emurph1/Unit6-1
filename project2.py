800d3c119e71f2a5250cb1e64c114d9d0d16424a
emurph1 <31485900+emurph1@users.noreply.github.com>
2018-01-04 14:38:06 -0500

    Updated from Brython Server: 1/4/2018 2:38:18 PM

--- project2.py
+++ project2.py
@@ -55,6 +55,7 @@ if __name__ == '__main__':
                         answ = input(p + ' , What word from the dictionary were you thinking of? ')
                         for word in loadD():
                         if answ == word:
+                            print('emily')
                             print(oP + 'Loses and ' + p + 'wins!')
                             replay = input('Would you like to play again? ')
                             if replay == 'yes':
