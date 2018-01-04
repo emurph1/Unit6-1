cef5c96236074671bc0909017f944bf76e0d0584
emurph1 <31485900+emurph1@users.noreply.github.com>
2018-01-04 14:23:55 -0500

    Updated from Brython Server: 1/4/2018 2:24:07 PM

--- project2.py
+++ project2.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
                         print(oP + 'Loses and ' + p + 'wins!')
                     else:
                         print('Either word is not in dictionary or is not a word and ' + oP + ' was bluffing' + ',' + p + 'wins!')
-                        replay = input('Would you like to play again?)
+                        replay = input('Would you like to play again? ')
                         if replay == 'yes':
                             i = 1
                         elif replay == 'no':
